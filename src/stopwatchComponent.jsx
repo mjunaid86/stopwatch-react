bcaa17c39e3da65c138633e7715746d6c6b0f945
muhammadjunaidkhan@airasia.com <muhammadjunaidkhan@airasia.com>
2022-12-27 23:43:09 +0800

    add css style

--- src/stopwatchComponent.jsx
+++ src/stopwatchComponent.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
-
-export  default function stopwatchComponent   ()   {
+import styles from "./stopwatchComponent.module.css";
+export default function stopwatchComponent() {
   const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [hour, setHour] = useState(0);
+
   const [isStart, setIsStart] = useState(false);
 
   const resetSecond = () => {
     setSecond(0);
+    setMinute(0);
+    setHour(0);
     setIsStart(false);
   };
 
@@ -17,7 +22,26 @@ export  default function stopwatchComponent   ()   {
   useEffect(() => {
     if (isStart) {
       const interval = setInterval(() => {
-        setSecond(second + 1);
+        //* Second *
+        if (second == 60) {
+          setSecond(1);
+
+          //* Minute *
+          if (minute == 60) {
+            setMinute(1);
+
+            //* Hour *
+            if (hour == 12) {
+              setHour(0);
+            } else {
+              setHour(hour + 1);
+            }
+          } else {
+            setMinute(minute + 1);
+          }
+        } else {
+          setSecond(second + 1);
+        }
       }, 1000);
 
       return () => clearInterval(interval);
@@ -26,14 +50,21 @@ export  default function stopwatchComponent   ()   {
 
   return (
     <div>
-      <div>{second}</div>
+      <div className={`${styles.parent}`}>
+        <div className={`${styles.child}`}>{('0' +hour).slice(-2)} </div>
+        <div className={`${styles.child}`}>:</div>
+        <div className={`${styles.child}`}>{('0' +minute).slice(-2)}</div>
+        <div className={`${styles.child}`}>:</div>
+        <div className={`${styles.child}`}>{('0' +second).slice(-2)}</div>
+      </div>
 
       <button style={{ marginRight: "10px" }} onClick={() => resetSecond()}>
         Reset
       </button>
-      <button onClick={() => stop()}> {isStart ? "Stop" : "Start"} </button>
+      <button className={`${isStart ? styles.stop : styles.start}`} onClick={() => stop()}>
+         
+        {isStart ? "Stop" : "Start"}{" "}
+      </button>
     </div>
   );
-};
-
- 
+}
